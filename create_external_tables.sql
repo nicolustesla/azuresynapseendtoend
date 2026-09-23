118332171cfd6568c87e38b4659939561d558f0f
Manoj kumar <147530096+nicolustesla@users.noreply.github.com>
2026-09-23 19:04:10 +0530

    Update external data source and table schema

--- create_external_tables.sql
+++ create_external_tables.sql
@@ -1,27 +1,34 @@
--- sql table
+-- Create External Data Source
 
 CREATE EXTERNAL DATA SOURCE blob_retail_datasource
 WITH (
-    LOCATION = 'https://pocretail.blob.core.windows.net/retail'
+    LOCATION = 'https://synapsesgacc.blob.core.windows.net/synasstorage'
 );
 
+
+-- Create External File Format
 
 CREATE EXTERNAL FILE FORMAT ParquetFormat
 WITH (
     FORMAT_TYPE = PARQUET
 );
 
+
+-- Create External Table
+
 CREATE EXTERNAL TABLE daily_revenue (
     event_date DATE,
     daily_revenue FLOAT,
-    total_purchases INT
+    total_purchases BIGINT
 )
 WITH (
-    LOCATION = '/gold/',  -- Important: folder path only
+    LOCATION = '/gold/',
     DATA_SOURCE = blob_retail_datasource,
     FILE_FORMAT = ParquetFormat
 );
 
 
+-- Read the data
 
-select * from daily_revenue
+SELECT *
+FROM daily_revenue;
